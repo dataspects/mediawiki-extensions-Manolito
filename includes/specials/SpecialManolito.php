86febc1d5970cbc4b0a98cd266f2b759ce8dec1f
Lex Sulzer <lex@dataspects.com>
2022-11-08 15:30:16 +0100

    SpecialManolito

---
+++ includes/specials/SpecialManolito.php
@@ -0,0 +1,20 @@
+<?php
+
+class SpecialManolito extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'Manolito' );
+	}
+
+	function execute( $par ) {
+		$request = $this->getRequest();
+		$output = $this->getOutput();
+		$this->setHeaders();
+		$output->addHTML( 'Hello' );
+		$output->addJsConfigVars(array(
+			'wgServer' => $GLOBALS['wgServer'],
+			'user' => $this->getUser()->getName()
+		));
+		$output->addModules( 'ext.manolito' );
+	}
+
+}
